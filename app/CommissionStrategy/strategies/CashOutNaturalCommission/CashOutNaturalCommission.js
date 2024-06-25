c4b20f36428a88bbc502794ca28da03d699fccd1
Alex Nepsha <nepsha1986@gmail.com>
2024-06-25 15:00:15 +0300

    CashOutCommission draft

--- app/CommissionStrategy/strategies/CashOutNaturalCommission/CashOutNaturalCommission.js
+++ app/CommissionStrategy/strategies/CashOutNaturalCommission/CashOutNaturalCommission.js
@@ -1,10 +1,16 @@
 const Commission = require("../Commission");
 const configService = require("../../../ConfigService/ConfigService");
-const round = require("../../../utils/round");
+const { round } = require("../../../utils");
 
 class cashOutNaturalCommission extends Commission {
-  async calculate(transaction, prevTransactions) {
+  async calculate(transaction, related) {
     const config = await configService.getCashOutNaturalConfig();
+
+    const spentAmount = related.reduce((acc, cur) => {
+      return acc + cur.operation.amount;
+    }, 0);
+
+    if(spentAmount + transaction.operation.amount < config.week_limit.amount) return 0;
 
     const { amount } = transaction.operation;
     return round(config.percents * (amount / 100));
